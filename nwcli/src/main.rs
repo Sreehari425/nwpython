ca92c91a842ed83d0f4cc992a5d489d130873391
Sreehari Anil <sreehari7102008@gmail.com>
2025-08-23 16:42:28 +0530

    dev: added reverse transpile

--- nwcli/src/main.rs
+++ nwcli/src/main.rs
@@ -5,7 +5,7 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: nwcli <source.nwpy> [--run]");
+        eprintln!("Usage: nwcli <source.nwpy|source.py> [--run] [--reverse-transpile]");
         process::exit(1);
     }
     let filename = &args[1];
